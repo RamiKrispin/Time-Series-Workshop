e096fcce6346af0c277000c500b027fb12e63461
RamiKrispin <ramkrisp@umich.edu>
2019-10-05 11:35:39 -0700

    edit the html file

--- Required packages.R
+++ Required packages.R
@@ -1,6 +1,6 @@
 # Required packages
 
-install.packages("forecast", "plotly", "ggplot2", "dplyr", "UKgrid", "fpp2", "shiny", "tsibble", "dygraph")
+install.packages(c("forecast", "plotly", "ggplot2", "dplyr", "UKgrid", "fpp2", "shiny", "tsibble", "dygraph"))
 
 devtools::install_github("RamiKrispin/TSstudio")
 devtools::install_github("RamiKrispin/forecastML")
