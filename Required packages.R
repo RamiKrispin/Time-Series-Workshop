5a6a65519d06abb56588e21a6dd27dde20dc9207
RamiKrispin <ramkrisp@umich.edu>
2019-10-05 08:31:03 -0700

    required packages

---
+++ Required packages.R
@@ -0,0 +1,6 @@
+# Required packages
+
+install.packages("forecast", "plotly", "ggplot2", "dplyr", "UKgrid", "fpp2", "shiny", "tsibble", "dygraph")
+
+devtools::install_github("RamiKrispin/TSstudio")
+devtools::install_github("RamiKrispin/forecastML")
